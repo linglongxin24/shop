997169db5b509c692fc24543d0b7910b2dbbc648
linglongxin24 <linglongxin24@163.com>
2020-02-16 17:53:43 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/msite',
+      component: MSite
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    }
+  ]
+})
